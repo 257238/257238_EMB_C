f5534e3fd7520126719b8131dafa062fe6c7751f
257238 <80746610+257238@users.noreply.github.com>
2021-04-30 14:54:44 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -1,53 +1,54 @@
-/**
- * @file project_main.c
- * @author Bharath.G ()
- * @brief Project to Blink an LED at 1000ms ON and 500 ms OFF Interval
- * @version 0.1
- * @date 2021-04-23
- * 
- * @copyright Copyright (c) 2021
- * 
- */
-#include "project_config.h"
 
-#include "user_utils.h"
-#include "blinky.h"
+#include "activity1.h"
+#include "activity2.h"
+#include "activity3.h"
+#include "activity4.h"
+#include<avr/io.h>
 
-/**
- * @brief Initialize all the Peripherals and pin configurations
- * 
- */
 void peripheral_init(void)
 {
-	/* Configure LED Pin */
-	DDRB |= (1 << DDB0);
+    /*Configure LED and Switch pins*/
+    InitializeLED();
+    /*Configure ADC registers and pins*/
+    InitializeADC();
+    /*Configure PWM registers and pins*/
+    InitializePWM();
+    /*Configure UART serial communication pin*/
+    InitializeUART(103);
 }
-
-void change_led_state(uint8_t state)
-{
-	LED_PORT = (state << LED_PIN);
-}
-
-
-/**
- * @brief Main function where the code execution starts
- * 
- * @return int Return 0 if the program completes successfully
- * @note PORTB0 is in sink config. i.e when pin is Low, the LED will turn OFF
- * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
- */
+    
+   
+uint16_t temp;
+char temp_data;
 int main(void)
 {
-	/* Initialize Peripherals */
-	peripheral_init();
+    /*uint16_t temp;*/
+    // Initialize peripherals
+    peripheral_init();
+    
+    while(1)
+    {
+        if(SENSOR_ON) //If switch_1 is ON
+        {
+            if(HEAT_ON) //If switch_2 is ON
+            {
+                activity1(LED_ON);//LED is ON
+                temp=ADCread(0);
+                temp_data = PWMout(temp);
+                UARTwrite(temp_data);
 
-	for(;;)
-	{
-        change_led_state(LED_ON);
-		delay_ms(LED_ON_TIME);
-		
-        change_led_state(LED_OFF);
-		delay_ms(LED_OFF_TIME);	
-	}
-	return 0;
+            }
+            else
+            {
+                
+                activity1(LED_OFF);
+            }
+        }
+        else
+        {
+            activity1(LED_OFF);//LED is OFF
+            OCR1A=0;
+        }
+    }
+    return 0;
 }
