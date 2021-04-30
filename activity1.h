add9a7a70ec19dfbcda7a379470d03e458073dd1
257238 <80746610+257238@users.noreply.github.com>
2021-04-30 14:40:49 +0530

    Add files via upload

---
+++ activity1.h
@@ -0,0 +1,20 @@
+#ifndef __ACTIVITY1_H_
+#define __ACTIVITY1_H_
+
+#include <avr/io.h>
+#include "activity1.h"
+
+
+#define LED_ON 	(0x01)			/**< LED state HIGH */
+#define LED_OFF	(0x00)			/**< LED state LOW */
+
+
+#define LED_PORT (PORTB)    /**< LED Port Number */
+#define LED_PIN  (PORTB0)   /**< LED Pin number  */
+#define SENSOR_ON !(PIND&(1<<PD0)) // Switch1 ON
+#define HEAT_ON !(PIND&(1<<PD1)) // Switch2 ON
+
+void activity1(uint8_t state);
+void InitLED(void);
+
+#endif
---
+++ activity4.h
@@ -0,0 +1,12 @@
+#ifndef _ACTIVITY4_H__
+#define __ACTIVITY4_H__
+
+#include<avr/io.h>
+
+void Initialize(uint16_t ubrr);
+
+char Read();
+/
+void Write(char data);
+
+#endif
